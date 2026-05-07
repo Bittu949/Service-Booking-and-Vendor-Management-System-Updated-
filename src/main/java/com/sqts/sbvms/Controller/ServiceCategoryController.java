4b4343fd423f503144f8b4f74002c2a44df63c62
BalkrishnaNaik <naikbalkrishna296@gmail.com>
2026-05-07 16:00:55 +0530

    Added service method for adding service

--- src/main/java/com/sqts/sbvms/Controller/ServiceCategoryController.java
+++ src/main/java/com/sqts/sbvms/Controller/ServiceCategoryController.java
@@ -6,6 +6,8 @@ import com.sqts.sbvms.Service.ServiceCategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,11 @@ import java.time.LocalDateTime;
 
 @RestController
 public class ServiceCategoryController {
-    private ServiceCategoryService serviceCategoryService;
+    private final ServiceCategoryService serviceCategoryService;
     public ServiceCategoryController(ServiceCategoryService serviceCategoryService){
         this.serviceCategoryService = serviceCategoryService;
     }
+    @PostMapping("/services")
     public ResponseEntity<ApiResponse<ServiceCategory>> createService(@Valid @RequestBody ServiceCategory service){
         return new ResponseEntity<>(
                 new ApiResponse<>(
--- src/main/java/com/sqts/sbvms/Service/ServiceCategoryService.java
+++ src/main/java/com/sqts/sbvms/Service/ServiceCategoryService.java
@@ -1,7 +1,18 @@
 package com.sqts.sbvms.Service;
 
+import com.sqts.sbvms.Entity.ServiceCategory;
+import com.sqts.sbvms.Exception.InvalidInputException;
+import com.sqts.sbvms.Repository.ServiceCategoryRepository;
 import org.springframework.stereotype.Service;
-
 @Service
 public class ServiceCategoryService {
+    ServiceCategoryRepository serviceCategoryRepository;
+    public ServiceCategoryService(ServiceCategoryRepository serviceCategoryRepository) {
+        this.serviceCategoryRepository = serviceCategoryRepository;
+    }
+    public ServiceCategory createService(ServiceCategory service){
+        if(service==null)
+            throw new InvalidInputException("Please fill all the fields.");
+        return serviceCategoryRepository.save(service);
+    }
 }
