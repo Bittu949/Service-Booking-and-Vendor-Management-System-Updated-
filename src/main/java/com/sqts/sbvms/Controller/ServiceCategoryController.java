4979376043ac5735c38e0b103ab7dc338eb7c0a9
BalkrishnaNaik <naikbalkrishna296@gmail.com>
2026-05-07 15:46:00 +0530

    Created createService method for creating service

--- src/main/java/com/sqts/sbvms/Controller/ServiceCategoryController.java
+++ src/main/java/com/sqts/sbvms/Controller/ServiceCategoryController.java
@@ -1,7 +1,29 @@
 package com.sqts.sbvms.Controller;
 
+import com.sqts.sbvms.Dto.ApiResponse;
+import com.sqts.sbvms.Entity.ServiceCategory;
+import com.sqts.sbvms.Service.ServiceCategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class ServiceCategoryController {
+    private ServiceCategoryService serviceCategoryService;
+    public ServiceCategoryController(ServiceCategoryService serviceCategoryService){
+        this.serviceCategoryService = serviceCategoryService;
+    }
+    public ResponseEntity<ApiResponse<ServiceCategory>> createService(@Valid @RequestBody ServiceCategory service){
+        return new ResponseEntity<>(
+                new ApiResponse<>(
+                        true,
+                        "Service created.",
+                        serviceCategoryService.createService(service),
+                        LocalDateTime.now()),
+                HttpStatus.CREATED);
+    }
 }
