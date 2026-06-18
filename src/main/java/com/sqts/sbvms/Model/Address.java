1e49bdf581a6b7e9c1baecdf328e3970ade0d28d
BalkrishnaNaik <naikbalkrishna296@gmail.com>
2026-06-18 13:54:54 +0530

    Added not blank annotation in Address class for validation

--- src/main/java/com/sqts/sbvms/Model/Address.java
+++ src/main/java/com/sqts/sbvms/Model/Address.java
@@ -1,6 +1,7 @@
 package com.sqts.sbvms.Model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Address {
+    @NotBlank
     private String addressLine1;
     private String addressLine2;
+    @NotBlank
     private String city;
+    @NotBlank
     private String state;
+    @NotBlank
     private String pincode;
 }
