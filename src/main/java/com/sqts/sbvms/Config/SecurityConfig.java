9e98af4d5fb33349707740ffc0328e7a4b9daa9c
BalkrishnaNaik <naikbalkrishna296@gmail.com>
2026-06-20 13:38:22 +0530

    Added BcryptPasswordEncode bean inside SecurityConfig

--- src/main/java/com/sqts/sbvms/Config/SecurityConfig.java
+++ src/main/java/com/sqts/sbvms/Config/SecurityConfig.java
@@ -1,5 +1,16 @@
 package com.sqts.sbvms.Config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
